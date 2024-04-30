cf9b1e2e9743bda511f205292e1ed4c4448bb911
ta truong <chom9x@gmail.com>
2024-04-30 14:48:30 +0700

    qqqqqqqqq

---
+++ app/register/page.tsx
@@ -0,0 +1,8 @@
+
+export default function Register() {
+    return (
+        <div className="flex min-h-screen flex-col items-center justify-between p-24">
+            qwe
+        </div>
+    )
+}
